4e2f05b24e43c0a154f65a6cb6a4caa6fbc9746b
Nommady <luiz.c.s.biologia@gmail.com>
2020-04-28 14:46:43 -0300

    Ajustes do no desafio 2

--- Desafio2.js
+++ Desafio2.js
@@ -13,32 +13,32 @@ var cento = ["Cento"]
 if (num.length == 1) {
     console.log(unidades[num]);
 }
-//de 10 a 19
 else if (num.length == 2 && num[0] == 1 && num[1] == 0) {
     console.log(dezenas[0])
 } else if (num.length == 2 && num[0] == 1) {
     console.log(especiais[num[1] - num[0]])
 
-}
-else if (num.length == 2) {//de 10 a 99    
+}else if (num.length == 2 && num[1] == 0 ) { //dezenas
+    var num2 = parseInt(num[0])  
+    console.log(dezenas[num2-1])
+}else if (num.length == 2) {//de 11 a 99    
+
     var num2 = parseInt(num[0])
     console.log(dezenas[num2 - 1], "e", unidades[num[1]])
-}
 
-else if (num.length == 3 && num[0] == 1 && num[1] == 0 && num[2] == 0) {//cem
-    console.log(centenas[1])
-} else if (num.length == 3) {// de 101 a 109
-    var num2 = parseInt(num[0])
-    var num3 = parseInt(num[1])
-    console.log(cento[0], "e",dezenas[num3 - 1],"e",unidades[num[2]])//corrigir isso!!!
+}else if (num.length == 3 && num[0] == 1 && num[1] == 0 && num[2] == 0) {//cem
+    console.log(centenas[0])
 } 
-
-/*else if (num.length == 3 && num[0] == 1 && num[1] == 1 && num[2] == 0) {// de 110 a 119
-    console.log(centenas[0], "e", dezenas[0])
-} else if (num.length == 3 && num[0] == 1 && num[1] == 1) {
-    console.log(centenas[0], "e", especiais[num[2] - num[0]])
-} else if (num.length == 3 && num[0] == 1 && num[1] == 2 && num[2] == 0) {// de 120 a 129
-    console.log(centenas[0], "e", dezenas[1])
-} else if (num.length == 3 && num[0] == 1 && num[1] == 2) {
-    console.log(centenas[0], "e", dezenas[1], "e", unidades[num[2]])
-}*/
+else if (num.length == 3 && num[1] == 1) {
+    var num2 = parseInt(num[2])
+       console.log(cento[0], "e",especiais[num2 - 1])
+} 
+else if (num.length == 3 && num[1] == 1) {
+    var num2 = parseInt(num[1])
+       console.log(cento[0], "e",dezenas[num2 - 1])
+}else if (num.length == 3) {
+    var num1 = parseInt(num[0])
+    var num2 = parseInt(num[1])
+    var num3 = parseInt(num[2])
+    console.log(centenas[num1 -1], "e",dezenas[num2 - 1],"e",unidades[num[2]])
+} 
