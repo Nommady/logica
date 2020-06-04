e2e3d4d8a94704aadc99e0df510fc7e960684a81
Jorge Luiz Carvalho <luiz.c.s.biologia@gmail.com>
2020-06-03 21:50:52 -0300

    Delete ExercicioInt4.js

--- ExercicioInt4.js
+++
@@ -1,9 +0,0 @@
-var rs = require("readline-sync")
-
-var sal = rs.questionFloat("Qual o valor do seu salario? ")
-var emp = rs.questionFloat("Qual o valor do seu emprestimo? ")
-var prest = rs.questionFloat("Em quantas prestacoes? ")
-var vparc = (emp / prest) 
-if (vparc >= ((sal/100) *30)){
-    console.log("Emprestimo negado")
-}else console.log("Emprestimo aprovado")
