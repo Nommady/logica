0326a156af25e1344be8e45f35626a15f1f33727
Jorge Luiz Carvalho <luiz.c.s.biologia@gmail.com>
2020-06-03 21:49:24 -0300

    Delete Exercicio12.js

--- Exercicio12.js
+++
@@ -1,9 +0,0 @@
-var rs = require("readline-sync")
-
-var sex = rs.questionInt("Digite 2 para escolher o sexo maculino e 1 para escolher o sexo feminino: ")
-var estado = rs.question("Qual seu estado civil?")
-
-if (sex == 1 && estado == "casada"){
-    var tempo = rs.questionInt("Quanto tempo de casada?  ")
-    console.log("fim do questionario ")
-}
