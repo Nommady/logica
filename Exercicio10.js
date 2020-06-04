b329d0aa5177dfa8403ed3de46235671819dc2ec
Jorge Luiz Carvalho <luiz.c.s.biologia@gmail.com>
2020-06-03 21:49:08 -0300

    Delete Exercicio10.js

--- Exercicio10.js
+++
@@ -1,7 +0,0 @@
-var rs = require("readline-sync")
-
-var num = rs.questionInt("Digite um numero! ")
-
-if (num >= 100 && num >= 200){
-    console.log("Seu numero esta entre 100 e 200")
-}else console.log("Seu numero nao esta entre 100 e 200")
