91f541b5a8df1368eb84df4c3cdd45fc6fb69714
Jorge Luiz Carvalho <luiz.c.s.biologia@gmail.com>
2020-06-03 21:51:15 -0300

    Delete ExercicioInt7.js

--- ExercicioInt7.js
+++
@@ -1,7 +0,0 @@
-var rs = require("readline-sync")
-
-var senha = rs.question("Digite a senha: ")
-
-if (senha == "batatafrita"){
-    console.log("ACESSO PERMITIDO")
-}else console.log("ACESSO NEGADO")
